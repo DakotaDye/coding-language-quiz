e52cb87fc00b89cc02cacd53b70a203fe4cf47fc
Raven Dye <ravenblackthorn139@gmail.com>
2022-06-23 21:29:47 -0700

    it will have if,else if and else

--- js/script.js
+++ js/script.js
@@ -13,6 +13,21 @@ window.addEventListener("load", function(){
     let form = document.querySelector("quiz")
     form.addEventListener("submit", function(event){
         event.preventDefault();
-        const 
+        const experience = document.querySelector("input[name='experience']:checked").value;
+        const intentions = document.querySelector("input[name= 'intentions']:checked").value;
+        if (experience === "No experience" && intentions === "simple web development") {
+            javaScript.removeAttribute("style");
+            python.style.display = "none";
+            ruby.style.display = "none";
+        } else if (experience === "plenty of experience" && intentions === "I'm even cooler so full software engineer for me!");{
+            python removeAttribute("style");
+            javaScript.style.display = "none";
+            ruby.style.display = "none";
+        } else {
+            ruby.removeAttribute("style");
+            javaScript.style.display = "none";
+            python.style.display = "none";
+        };
+        
     })
 })
